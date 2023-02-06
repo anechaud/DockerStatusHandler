ef8b0b43e270d469db85bcab86399aca2f32378d
Aneesha <anee.guria@gmail.com>
2023-02-06 21:11:39 +0100

    some refactoring

--- DockerStatusHandler/MonitorContainer.cs
+++ DockerStatusHandler/MonitorContainer.cs
@@ -19,12 +19,9 @@ namespace DockerStatusHandler
                 Progress<Message> progress = new Progress<Message>();
                 progress.ProgressChanged += async (sender, message) =>
                 {
-                    Console.WriteLine($"{message.Actor.ID} -- {message.Status}");
-                    if(!string.IsNullOrEmpty(message.Status) && message.Status.Equals("stop", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                       await _dockerManager.RemoveContainer(message.ID);
-                        Console.WriteLine($"Container {message.ID} is removed");
-                    }
+                    MonitorComputation(message);
+                    MonitorContainers(message);
+                    await DeleteContainerAsync(message);
                 };
 
                 await _dockerManager.MonitorContainerEvents(new ContainerEventsParameters(), progress: progress);
@@ -32,6 +29,27 @@ namespace DockerStatusHandler
             catch (Exception ex)
             {
                 Console.WriteLine("Something went wrong while monitorning status", ex.Message);
+            }
+        }
+
+        private void MonitorComputation(Message message)
+        {
+            string val = string.Empty;
+            var exitCode = message.Actor?.Attributes.TryGetValue("exitCode", out val);
+            Console.WriteLine(val);
+        }
+
+        private void MonitorContainers(Message message)
+        {
+            Console.WriteLine($"{message.Actor.ID} -- {message.Status}");
+        }
+
+        private async Task DeleteContainerAsync(Message message)
+        {
+            if (!string.IsNullOrEmpty(message.Status) && message.Status.Equals("stop", StringComparison.InvariantCultureIgnoreCase))
+            {
+                await _dockerManager.RemoveContainer(message.ID);
+                Console.WriteLine($"Container {message.ID} is removed");
             }
         }
     }
